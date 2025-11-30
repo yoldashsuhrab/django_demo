cef84d050192ec2f397d9b70d83dd99fcf6651e3
Suhrab Yoldash <suhrabyoldash@SMA.local>
2025-11-30 16:24:11 +0500

    environ keys2

--- goodledemologin/settings.py
+++ goodledemologin/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-znsns_-37^wglt3y$6p83s#6+8x%+i8lk+3$vctmn2dxg4o-8h'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
